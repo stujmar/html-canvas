2f748cb661cd63971946b5424560cf71917366ce
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-06-16 18:24:34 -0500

    [sm] got characters to move and jump and duck

---
+++ fight/models/Keys.js
@@ -0,0 +1,28 @@
+export class Keys {
+  constructor () {
+    this.d = {
+      pressed: false
+    };
+    this.a = {
+      pressed: false
+    };
+    this.w = {
+      pressed: false
+    };
+    this.s = {
+      pressed: false
+    }
+    this.ArrowLeft = {
+      pressed: false
+    };
+    this.ArrowRight = {
+      pressed: false
+    };
+    this.ArrowUp = {
+      pressed: false
+    };
+    this.ArrowDown = {
+      pressed: false
+    }
+  }
+}
