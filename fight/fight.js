a5fa799c00d27d298e60a10594ed88826636cd1f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-06-15 22:09:43 -0500

    [sm] started to make canvas fighting game, very cool

---
+++ fight/fight.js
@@ -0,0 +1,24 @@
+console.log("fight.js loaded");
+
+const canvas = document.querySelector("#fight-canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0,0,canvas.width,canvas.height);
+
+class Sprite {
+    constructor(position) {
+        this.position = position;
+    }
+    draw() {
+      c.fillStyle = "red";
+      c.fillRect(this.position.x, this.position.y, 50, 150);
+    }
+
+  }
+
+  const player = new Sprite({x: 0, y: 0});
+  player.draw();
+  console.log(player);
