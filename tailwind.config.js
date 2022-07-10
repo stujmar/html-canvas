3fdccf23df20e81b1c0e6beb5d71e072bcf1d223
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-07-09 22:08:42 -0500

    [sm] fixed some tailwind issues and started to build out multicanvas tests

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./fight/**/*.{html,js}", "./index.html"],
+  content: [
+    "./fight/**/*.{html,js}", "./index.html",
+    "./multi-canvas/**/*.{html,js}", "./index.html",
+  ],
   theme: {
     extend: {},
   },
